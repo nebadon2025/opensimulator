27128c53f2b1a6c9e18181f293abe6b21aac57f7
jmalthus <jmalthus>
2007-02-16 22:12:25 +0000

    Imported gareth's ogs branch thingy

---
+++ gridserver/gridserver_config.inc.php
@@ -0,0 +1,12 @@
+<?
+// All the grid server specific stuff lives here
+
+// What we send to authenticate to the user/login server
+$userserver_sendkey="YO LOGIN, I'm ThE ReAL thInG and CaSE SeNSiTIVe TOO! Random stuff is good in our keys - 2432324343ifvo3o332i32o32i32o";
+
+// What we expect to get back from the user/login server
+$userserver_recvkey="HEY GRID SERVER, I AM THE DAMN LOGIN SERVER, THIS IS MY KEY! ooh, randomness 32432gfrgergregrgrt34to44i433igfh44t09i094r09";
+
+$sim_recvkey = "we are not linden labs1234dyson";
+$sim_sendkey = "you are ogs4325678";
+?>
---
+++ login/login_config.inc.php
@@ -0,0 +1,10 @@
+<?
+// All the user/login server specific stuff lives here
+
+// What we send to authenticate to the grid server
+$gridserver_sendkey="HEY GRID SERVER, I AM THE DAMN LOGIN SERVER, THIS IS MY KEY! ooh, randomness 32432gfrgergregrgrt34to44i433igfh44t09i094r09";
+
+// What we expect to get back from the grid server
+$gridserver_recvkey="YO LOGIN, I'm ThE ReAL thInG and CaSE SeNSiTIVe TOO! Random stuff is good in our keys - 2432324343ifvo3o332i32o32i32o";
+
+?>

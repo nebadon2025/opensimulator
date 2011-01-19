b3179d34dabe8360d7d0549941e1e1a9fb7cfa3e
Huaiyu (Kitty) Liu <huaiyu.liu@intel.com>
2011-01-19 10:52:06 -0800

    reimplement SceneObjectGroup.UpdateObjectAllProperties to do parts updating differently, as an attemp to make linkset updates correct, and rename the function UpdateObjectGroupBySync.

--- OpenSim/Region/Framework/Scenes/SceneGraph.cs
+++ OpenSim/Region/Framework/Scenes/SceneGraph.cs
@@ -1945,7 +1945,7 @@ namespace OpenSim.Region.Framework.Scenes
                 if (entity is SceneObjectGroup)
                 {
                     SceneObjectGroup localSog = (SceneObjectGroup)entity;
-                    Scene.ObjectUpdateResult updateResult = localSog.UpdateObjectAllProperties(updatedSog);
+                    Scene.ObjectUpdateResult updateResult = localSog.UpdateObjectGroupBySync(updatedSog);
                     return updateResult;
                 }
                 else

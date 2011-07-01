ac7537d24020af939a470618f35f2855709de594
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-07-01 21:37:03 +0100

    fix build break I just introduced

--- OpenSim/Region/Framework/Scenes/Scene.cs
+++ OpenSim/Region/Framework/Scenes/Scene.cs
@@ -2805,7 +2805,7 @@ namespace OpenSim.Region.Framework.Scenes
             client.OnMoveInventoryFolder -= HandleMoveInventoryFolder; // 2; //!!
             client.OnFetchInventoryDescendents -= HandleFetchInventoryDescendents;
             client.OnPurgeInventoryDescendents -= HandlePurgeInventoryDescendents; // 2; //!!
-            client.OnFetchInventory -= HandleFetchInventory;
+            client.OnFetchInventory -= m_asyncInventorySender.HandleFetchInventory;
             client.OnUpdateInventoryItem -= UpdateInventoryItemAsset;
             client.OnCopyInventoryItem -= CopyInventoryItem;
             client.OnMoveInventoryItem -= MoveInventoryItem;

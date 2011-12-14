903da8acbd6ef37a94e4eab8329f9fd4583ff2b3
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2011-12-14 18:33:44 +0000

    Simplify some manipulation of _taintedActors in OdeScene

--- OpenSim/Region/Physics/OdePlugin/OdeScene.cs
+++ OpenSim/Region/Physics/OdePlugin/OdeScene.cs
@@ -2614,12 +2614,9 @@ namespace OpenSim.Region.Physics.OdePlugin
                 OdeCharacter taintedchar = ((OdeCharacter)actor);
                 lock (_taintedActors)
                 {
-                    if (!(_taintedActors.Contains(taintedchar)))
-                    {
-                        _taintedActors.Add(taintedchar);
-                        if (taintedchar.bad)
-                            m_log.DebugFormat("[ODE SCENE]: Added BAD actor {0} to tainted actors", taintedchar.m_uuid);
-                    }
+                    _taintedActors.Add(taintedchar);
+                    if (taintedchar.bad)
+                        m_log.ErrorFormat("[ODE SCENE]: Added BAD actor {0} to tainted actors", taintedchar.m_uuid);
                 }
             }
         }
@@ -2700,14 +2697,10 @@ namespace OpenSim.Region.Physics.OdePlugin
                     {
                         lock (_taintedActors)
                         {
-                            if (_taintedActors.Count > 0)
-                            {
-                                foreach (OdeCharacter character in _taintedActors)
-                                    character.ProcessTaints();
+                            foreach (OdeCharacter character in _taintedActors)
+                                character.ProcessTaints();
 
-                                if (_taintedActors.Count > 0)
-                                    _taintedActors.Clear();
-                            }
+                            _taintedActors.Clear();
                         }
 
                         lock (_taintedPrims)

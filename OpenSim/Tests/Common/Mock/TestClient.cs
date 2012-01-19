26be9ed08e8a0a0be54e3e458836d0a946992690
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2012-01-19 21:57:12 +0000

    Add image not in database test for LLImageManager

--- OpenSim/Tests/Common/Mock/TestClient.cs
+++ OpenSim/Tests/Common/Mock/TestClient.cs
@@ -60,6 +60,7 @@ namespace OpenSim.Tests.Common.Mock
 
         public List<ImageDataPacket> SentImageDataPackets { get; private set; }
         public List<ImagePacketPacket> SentImagePacketPackets { get; private set; }
+        public List<ImageNotInDatabasePacket> SentImageNotInDatabasePackets { get; private set; }
 
 // disable warning: public events, part of the public API
 #pragma warning disable 67
@@ -454,6 +455,7 @@ namespace OpenSim.Tests.Common.Mock
 
             SentImageDataPackets = new List<ImageDataPacket>();
             SentImagePacketPackets = new List<ImagePacketPacket>();
+            SentImageNotInDatabasePackets = new List<ImageNotInDatabasePacket>();
         }
 
         /// <summary>
@@ -821,6 +823,10 @@ namespace OpenSim.Tests.Common.Mock
 
         public void SendImageNotFound(UUID imageid)
         {
+            ImageNotInDatabasePacket p = new ImageNotInDatabasePacket();
+            p.ImageID.ID = imageid;
+
+            SentImageNotInDatabasePackets.Add(p);
         }
 
         public void SendShutdownConnectionNotice()

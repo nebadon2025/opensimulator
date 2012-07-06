068917bc298ce46d44ac0294e2b2269d3bb79567
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2012-07-07 00:02:45 +0100

    Remove redundant SetScene() function in Scene.AddSceneObject()
    This is always done later on in SceneGraph.AddSceneObject() if the call hasn't failed due to sanity checks.
    There's no other purpose for this method to exist and it's dangerous/pointless to call in other conditions.

--- OpenSim/Data/Tests/RegionTests.cs
+++ OpenSim/Data/Tests/RegionTests.cs
@@ -1082,7 +1082,7 @@ namespace OpenSim.Data.Tests
             sop.Shape = PrimitiveBaseShape.Default;
 
             SceneObjectGroup sog = new SceneObjectGroup(sop);
-            sog.SetScene(scene);
+//            sog.SetScene(scene);
 
             return sog;
         }

761fe07f1ea738940e27c0565d9370b02973a5e9
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2013-04-03 00:48:36 +0100

    minor: Stop falsely logging that a teleport was being aborted on client logout even when no teleport was active.

--- OpenSim/Region/CoreModules/Framework/EntityTransfer/EntityTransferModule.cs
+++ OpenSim/Region/CoreModules/Framework/EntityTransfer/EntityTransferModule.cs
@@ -276,10 +276,8 @@ namespace OpenSim.Region.CoreModules.Framework.EntityTransfer
 
         private void OnConnectionClosed(IClientAPI client)
         {
-            if (client.IsLoggingOut)
+            if (client.IsLoggingOut && m_entityTransferStateMachine.UpdateInTransit(client.AgentId, AgentTransferState.Aborting))
             {
-                m_entityTransferStateMachine.UpdateInTransit(client.AgentId, AgentTransferState.Aborting);
-
                 m_log.DebugFormat(
                     "[ENTITY TRANSFER MODULE]: Aborted teleport request from {0} in {1} due to simultaneous logout", 
                     client.Name, Scene.Name);

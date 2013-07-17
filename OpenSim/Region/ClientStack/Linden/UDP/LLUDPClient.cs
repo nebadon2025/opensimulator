a94a43d24947e8fc4f9ba7a00267fbebdfb91990
Justin Clark-Casey (justincc) <jjustincc@googlemail.com>
2013-07-18 00:50:16 +0100

    Revert "Properly remove the hack queue update thread when the voewr shuts down"
    This reverts commit 7c544c0d4e4eab827f69acd90f47d593153117d6.

--- OpenSim/Region/ClientStack/Linden/UDP/LLUDPClient.cs
+++ OpenSim/Region/ClientStack/Linden/UDP/LLUDPClient.cs
@@ -223,7 +223,7 @@ namespace OpenSim.Region.ClientStack.LindenUDP
 
         private void ActOnEmptyQueue()
         {
-            while (IsConnected)
+            while (true)
             {
                 m_queueEmptyEvent.WaitOne();
                 FireQueueEmpty(m_categories);
